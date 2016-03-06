a6e8ab8bfd2c40a16c79841fee773734fd7fcf77
Müge Baydar <mugeeebaydar@gmail.com>
2016-03-07 00:19:28 +0200

    Veritabani
    SQLiteOpenHelper ve DatabaseHelper adinda iki class olusturuldu.
    DatabaseHelper'e veritabanini baglamak icin kodlar yazildi

--- app/src/main/java/com/example/omerf/kitapuygulamasi/MainActivity.java
+++ app/src/main/java/com/example/omerf/kitapuygulamasi/MainActivity.java
@@ -1,12 +1,18 @@
 package com.example.omerf.kitapuygulamasi;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -14,13 +20,18 @@ import android.widget.Toast;
 
 import org.xml.sax.InputSource;
 
+import java.io.IOException;
 import java.io.StringReader;
+import java.sql.SQLException;
+
 
 public class MainActivity extends AppCompatActivity {
     ListView list1;
     String baslik,aciklama;
     Button ekle,cikar;
     private ImageView resim;
+    DatabaseHelper dbHelper;
+    EditText ekleEditText,cikarEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,8 @@ public class MainActivity extends AppCompatActivity {
         list1 = (ListView) findViewById(R.id.list1);
         ekle = (Button) findViewById(R.id.ekle);
         cikar = (Button) findViewById(R.id.cikar);
+        ekleEditText = (EditText) findViewById(R.id.ekle_edtxt);
+        cikarEditText = (EditText) findViewById(R.id.cikar_edtxt);
 
 
         list1.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -90,16 +103,30 @@ public class MainActivity extends AppCompatActivity {
                     case "Kitap15":
                         aciklama = getResources().getString(R.string.Kitap15);
                         break;
-
-
                 }
                 builder.setMessage(aciklama);
                 builder.show();
             }
         });
+
+        dbHelper = new DatabaseHelper(this);
+        try{
+            dbHelper.createDataBase();
+            dbHelper.openDataBase();
+        }
+        catch(IOException e ){
+            e.printStackTrace();
+        }
+        catch (SQLException e) { // hata vermesi sonucu add ile program tarafýndan eklendi.
+            e.printStackTrace();
+        }
+
+
         ekle.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //EKLE BUTONUNU VERÝTABANINA BAGLA
+
                 Toast.makeText(MainActivity.this, "Eklendi", Toast.LENGTH_LONG).show();
             }
         });
---
+++ app/src/main/java/com/example/omerf/kitapuygulamasi/SQLiteOpenHelper.java
@@ -0,0 +1,7 @@
+package com.example.omerf.kitapuygulamasi;
+
+/**
+ * Created by bayda on 6.03.2016.
+ */
+public class SQLiteOpenHelper {
+}
